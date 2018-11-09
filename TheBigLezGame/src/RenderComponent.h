b9357a1bacd180d07c3e40abad4caf50124921f0
Niclas Jonsson <B00307864@studentmail.uws.ac.uk>
2018-11-09 13:46:24 +0000

    Little bit of code change

--- TheBigLezGame/src/RenderComponent.h
+++ TheBigLezGame/src/RenderComponent.h
@@ -1,9 +1,20 @@
 #pragma once
-
+#include <GL/glew.h>
+#include <iostream>
+#include <vector>
 class RenderComponent {
 private:
-
+	// Object 
+	std::vector<GLfloat> verts;
+	std::vector<GLfloat> norms;
+	std::vector<GLfloat> texCoords;
+	std::vector<GLfloat> indices;
+	GLfloat indexNum;
+	void loadObject(const char* mesh_filename);
 public:
+	RenderComponent(const char* mesh_filename) {
+
+	};
 	void Draw();
 
 };
